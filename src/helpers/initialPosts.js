b787e641a206b425a0f47d7099c722574eb9b26c
Mike Rachman <mikerachman1@gmail.com>
2023-01-30 11:17:18 -0500

    added one more post

---
+++ src/helpers/initialPosts.js
@@ -0,0 +1,16 @@
+const initialPosts = [
+  { username: "rockguy429",
+    caption: "Diabase is the coolest rock, very rare too",
+    imageUrl:"https://www.climbing.com/wp-content/uploads/2016/04/burr102713_291jpg.jpg?width=500" 
+  },
+  { username: "Sam Doltsmith",
+    caption: "Look at these slopers, Sandstone is amazing",
+    imageUrl:"https://images.squarespace-cdn.com/content/v1/5888e176d1758e783f89e2d6/1488057794769-IIJC9G26A3236DB9DO3D/DSC_3766.jpg?format=500w" 
+  },
+  { username: "rockguy429",
+    caption: "I have been climbing at Mt Gretna for many years now. It never ceases to amaze me the number of incredible boulders here.",
+    imageUrl:"https://bloximages.newyork1.vip.townnews.com/lancasteronline.com/content/tncms/assets/v3/editorial/6/65/66581ae7-8385-517a-8033-281e14e29de4/524dc29b29702.image.jpg" 
+  },
+];
+
+export default initialPosts;

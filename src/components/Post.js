6e57e9c4e6a3903d80ad32b69b5e998002af1645
Mike Rachman <mikerachman1@gmail.com>
2023-01-30 09:59:44 -0500

    created Post component jsx

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { Avatar } from "@material-ui/core";
 
-const Post = () => {
-
+const Post = ({ username, caption, imageUrl }) => {
+  return (
+    <div className="post">
+      <div className="post-header">
+        <Avatar
+          className="post-avatar"
+          alt={username}
+          src="/static/images/avatar/1.jpg"
+        />
+        <h3>{username}</h3>
+      </div>
+      <img className="post-image" src={imageUrl} alt="post" />
+      <h4 className="post-text">
+        <strong>{username}</strong>
+        {caption}
+       </h4>
+    </div>
+  );
 };
 
 export default Post;

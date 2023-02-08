afa38595f28559ba20d92623e05d5a346b14bce3
Mike Rachman <mikerachman1@gmail.com>
2023-02-08 14:11:10 -0500

    added eslint-disable

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { collection, getDocs } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "../firebase/FirebaseInit";

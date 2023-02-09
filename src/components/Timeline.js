6b6224484300f297cc1392e26861cbda0e563445
Mike Rachman <mikerachman1@gmail.com>
2023-02-09 14:14:50 -0500

    created Timeline to contain home posts display

---
+++ src/components/Timeline.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Post from "./Post";
+
+const Timeline = ({ posts, currentUser }) => {
+
+  return (
+    <div>
+      { !currentUser && 
+        <div className='notice'>
+          <h3>Log in or Sign up to add, like, and comment on Posts</h3>
+        </div>
+      }
+      {/* <button onClick={() => console.log(posts)}>log posts</button>
+      <button onClick={() => console.log(user)}>log user</button> */}
+
+      <div className='timeline'>
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            postId={post.id}
+            currentUser={currentUser}
+            username={post.data.username}
+            caption={post.data.caption}
+            imageUrl={post.data.imageUrl}
+            likes={post.data.likes}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Timeline;

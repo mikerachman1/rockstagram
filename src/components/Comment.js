6a25a4cdd23713c39027122f38c19419c3c54a30
Mike Rachman <mikerachman1@gmail.com>
2023-02-10 15:21:46 -0500

    rendered NewComment and queried comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,15 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { db } from "../firebase/FirebaseInit";
 
-const Comment = ({ postId }) => {
+import NewComment from "./NewComment";
+
+const Comment = ({ postId, currentUser }) => {
   const [comments, setComments] = useState([]);
 
   const fetchComments = async () => {
     const fetchedComments = [];
-    const querySnapshot = await getDocs(collection(db, "posts", `${postId}`, "comments"));
+    const q = query(collection(db, "posts", `${postId}`, "comments"), orderBy("timestamp"));
+    const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
       const commentObj = { id: doc.id, data: doc.data() };
       fetchedComments.push(commentObj);
@@ -24,15 +27,25 @@ const Comment = ({ postId }) => {
   }, [])
 
   return (
-    <div className="comments">
-      {comments.map((comment) => (
-        <div className="comment" key={comment.id}>
-          <Link to={`/user/${comment.data.username}`}>
-            <strong>{comment.data.username}</strong> 
-          </Link>
-          {comment.data.body}
-        </div>
-      ))}
+    <div>
+      <div className="comments">
+        {comments.map((comment) => (
+          <div className="comment" key={comment.id}>
+            <Link to={`/user/${comment.data.username}`}>
+              <strong>{comment.data.username}</strong>
+            </Link>
+            {comment.data.body}
+          </div>
+        ))}
+      </div>
+      { currentUser &&
+            <NewComment
+              currentUser={currentUser}
+              postId={postId}
+              comments={comments}
+              setComments={setComments}
+            />
+          }
     </div>
   );
 };
